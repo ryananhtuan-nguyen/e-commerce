27dac0d5b150d9e5873fe6fb13b153ec1fc70588
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-03 21:19:38 +1300

    nav bar

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  return (
+    <nav>
+      <Gutter>
+        <Link href="/">
+          <img src="/logo-black.svg" alt="logo" />
+        </Link>
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
