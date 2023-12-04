d2f0a3d338b8a0da9edf05e9d55fbb0814024c63
Ryan Nguyen <ryan.nguyen0720@gmail.com>
2023-12-05 12:09:41 +1300

    refactor: remove import

--- src/app/_heros/CustomHero/index.tsx
+++ src/app/_heros/CustomHero/index.tsx
@@ -1,9 +1,7 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 
 import { Page } from '../../../payload/payload-types'
-import { Gutter } from '../../_components/Gutter'
 import { CMSLink } from '../../_components/Link'
-import { Media } from '../../_components/Media'
 import RichText from '../../_components/RichText'
 
 import classes from './index.module.scss'
