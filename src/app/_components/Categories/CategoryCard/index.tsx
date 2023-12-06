c26805dccab4f5db7eee3a378cde4ed9d976cb89
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-06 23:32:14 +1300

    feat: Single category card

---
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+import { Category, Media } from '../../../../payload/payload-types'
+
+import classes from './index.module.scss'
+
+const CategoryCard = ({ category }: { category: Category }) => {
+  const media = category.media as Media
+  return (
+    <Link
+      href="/products"
+      className={classes.card}
+      style={{
+        backgroundImage: `url(${media.url})`,
+      }}
+    >
+      <p className={classes.title}>{category.title}</p>
+    </Link>
+  )
+}
+
+export default CategoryCard
