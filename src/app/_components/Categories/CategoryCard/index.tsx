5f8e3719ca353e0959f498fa2bc648f572c9a5d6
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2023-12-06 23:45:42 +1300

    feat: onClick Link to filter category

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,12 +1,17 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
 
 import { Category, Media } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
 
 const CategoryCard = ({ category }: { category: Category }) => {
   const media = category.media as Media
+
+  const { setCategoryFilters } = useFilter()
+
   return (
     <Link
       href="/products"
@@ -14,6 +19,7 @@ const CategoryCard = ({ category }: { category: Category }) => {
       style={{
         backgroundImage: `url(${media.url})`,
       }}
+      onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
     </Link>
